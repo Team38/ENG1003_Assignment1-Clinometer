04387022d0d1ef8be80aeb0c3a2855febb75ee95
Oscar Garrington <obgar1@student.monash.edu>
2015-09-04 10:41:32 +1000

    Added a Button for Swap between moving average and median
    Added a button to swap between two functions. And coded its
    functionality

--- Spirit Level/js/SpiritLevelProcessor.js
+++ Spirit Level/js/SpiritLevelProcessor.js
@@ -36,21 +36,22 @@ function SpiritLevelProcessor() {
         retVal,
         outputAngle = document.getElementById("message-area"),
         numClick = 0,
+        swapClick = 0,
         bufferRecord = {
-        x: [],
-        y: [],
-        z: []
-    };
-        
-   
+            x: [],
+            y: [],
+            z: []
+        };
+
+
     var uiController = null;
 
     self.initialise = function (controller) {
         uiController = controller;
 
         //phone window. This code will run handleMotion when it detect device's motion.
-        (window.addEventListener("devicemotion", handleMotion)); 
-       
+        (window.addEventListener("devicemotion", handleMotion))
+        window.onload = swapButton;
     }
 
     function handleMotion(event) {
@@ -65,8 +66,11 @@ function SpiritLevelProcessor() {
 
         var rawMotionData = [gX, gY, gZ];
 
-        retVal = (movingAverage(bufferRecord, rawMotionData));
-
+        if (swapClick % 2 === 0) {
+            retVal = (movingAverage(bufferRecord, rawMotionData));
+        } else {
+            retval = (movingMedian(bufferRecord, rawMotionData));
+        }
     }
 
     function movingAverage(buffer, newValue) {
@@ -124,24 +128,20 @@ function SpiritLevelProcessor() {
             y: sumY / buffer.y.length,
             z: sumZ / buffer.z.length
         };
-        
+
         var transValues = {
-            x: Number(filteredValues.x) * (dimensions.width/2 -10), //the 10px is to account for the size of the bubble (which is 20*20 px , then divide it by 2 so 10px CHECKED CSS FOR BUBBLE SIZE).
-            y: Number(filteredValues.y) * (dimensions.height/2),
+            x: Number(filteredValues.x) * (dimensions.width / 2 - 10), //the 10px is to account for the size of the bubble (which is 20*20 px , then divide it by 2 so 10px CHECKED CSS FOR BUBBLE SIZE).
+            y: Number(filteredValues.y) * (dimensions.height / 2),
         };
-     
-		uiController.bubbleTranslate(transValues.x,transValues.y,"dark-bubble");
-        
+
+        uiController.bubbleTranslate(transValues.x, transValues.y, "dark-bubble");
+
         if (numClick % 2 === 0) {
-        
-            uiController.bubbleTranslate(transValues.x,transValues.y,"pale-bubble");
-        
+
+            uiController.bubbleTranslate(transValues.x, transValues.y, "pale-bubble");
+
         }
-        
-        else {
-        
-        }
-        
+
         displayAngle(filteredValues.x, filteredValues.y, filteredValues.z);
 
         return transValues;
@@ -154,39 +154,56 @@ function SpiritLevelProcessor() {
         // Input: x,y,z
         //      These values should be the filtered values after the Moving Average for
         //      each of the axes respectively
-        var retVal = document.getElementById("message-area"),
+        var displayRef = document.getElementById("message-area"),
             finalAngle = Math.acos(z / (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)))) * 180 / Math.PI;
 
-        retVal.innerHTML = finalAngle.toFixed(2) + "&deg";
+        displayRef.innerHTML = finalAngle.toFixed(2) + "&deg from the Z-Axis";
 
     }
 
-   self.freezeClick = function () {
-       
+    self.freezeClick = function () {
+
         numClick++;
-       
-            if (numClick % 2 !== 0) {
-                uiController.bubbleTranslate(retVal.x,retVal.y,"pale-bubble");
-            }
-            
-            else {
-            
-            }
-       
-        }  
-   
-        function movingMedian(buffer, newValue) {
-            // ADVANCED FUNCTIONALITY
-            // =================================================================
-            // This function handles the Moving Median Filter
-            // Input:
-            //      buffer
-            //      The buffer in which the function will apply the moving to.
 
-            //      newValue
-            //      This should be the newest value that will be pushed into the buffer
-
-            // Output: filteredValue
-            //      This function should return the result of the moving average filter
+        if (numClick % 2 !== 0) {
+            uiController.bubbleTranslate(retVal.x, retVal.y, "pale-bubble");
         }
+
+    }
+
+    self.swap = function () {
+        var swapButtonRef = document.getElementById("swapButton");
+        swapClick++;
+        if (swapClick % 2 === 0)
+            swapButtonRef.innerHTML = "Use Moving Median";
+        else
+            swapButtonRef.innerHTML = "Use Moving Average";
+    }
+
+    function swapButton() {
+        var bottomDivNode = document.getElementById("bottom-div"),
+            tempNode = document.createElement("button");
+        tempNode.setAttribute("id", "swapButton");
+        tempNode.setAttribute("class", "ui-btn ui-corner-all");
+        tempNode.setAttribute("onClick", "spiritLevelProcessor.swap()");
+        tempNode.innerHTML = ("Use Moving Median");
+        bottomDivNode.appendChild(tempNode);
+
+
+    }
+
+    function movingMedian(buffer, newValue) {
+        // ADVANCED FUNCTIONALITY
+        // =================================================================
+        // This function handles the Moving Median Filter
+        // Input:
+        //      buffer
+        //      The buffer in which the function will apply the moving to.
+
+        //      newValue
+        //      This should be the newest value that will be pushed into the buffer
+
+        // Output: filteredValue
+        //      This function should return the result of the moving average filter
+    }
 }
